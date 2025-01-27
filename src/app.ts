5f73dde714bcbb001636fdbcb70004be7133a12f
Mikołaj Sławnikowski <mikolaj.slawnikowski@gmail.com>
2025-01-27 20:58:17 +0100

    Dokumentacja WIP

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,34 @@ import fruitsRoutes from "./routes/fruitsRoutes";
 import arcsRoutes from "./routes/arcsRoutes";
 import typeDefs from "./graphQL/schema";
 import resolvers from "./graphQL/resolvers";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 
 const app: Application = express();
+const swaggerDocument = YAML.load(path.join(__dirname, "./docs/openapi.yaml"));
 
 app.use(express.json());
 app.use(corsMiddleware);
 app.use(setHeaders);
+
+app.get("/api-docs/swagger.json", (req, res) => {
+  res.json(swaggerDocument);
+});
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    explorer: true,
+    customCss: ".swagger-ui .topbar { display: none }",
+    swaggerOptions: {
+      persistAuthorization: true,
+      docExpansion: "list",
+      url: "/api-docs/swagger.json",
+    },
+  })
+);
 
 app.use("/characters", charactersRoutes);
 app.use("/fruits", fruitsRoutes);
---
+++ src/docs-website/docs/intro.md
@@ -0,0 +1,26 @@
+---
+sidebar_position: 1
+---
+
+# TAPI Introduction
+
+API providing information about characters, fruits, and arcs from the One Piece universe.
+
+## Technologies Used
+
+- **Backend**:
+
+  - Node.js with Express
+  - GraphQL for flexible queries
+  - REST API for traditional endpoints
+
+- **Documentation**:
+  - OpenAPI/Swagger
+  - Docusaurus
+
+## Available Services
+
+1. [REST API](/docs/category/rest-api)
+2. [GraphQL API](/docs/category/graphql)
+
+## Getting Started
